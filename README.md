c874b81c3f62c766f1315286ac06dc4371136ca5
Erick Adrian Perez <erick.perez@plansource.com>
2023-10-15 21:43:32 -0600

    EAP commit with all my files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # eap-webpack
-# eap-webpack
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// import generateJoke from './generateJoke'
+// import './styles/main.scss'
+// import laughing from './assets/laughing.svg'
+import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/js/bootstrap-min.js'
+
+
+// const laughImg = document.getElementById('laughImg')
+// laughImg.src = laughing
+
+// const jokeBtn = document.getElementById('jokeBtn')
+// jokeBtn.addEventListener('click', generateJoke)
+
+// generateJoke()
+
+console.log('Erick is the Man!')
